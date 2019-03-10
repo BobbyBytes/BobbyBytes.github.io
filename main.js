b944a838dd06e71597f09b7c75286977663ae86d
Bobby <robbie234@gmail.com>
2019-03-10 19:45:01 -0400

    test

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ document.getElementById('batchInputForm').addEventListener('submit', saveBatch);
 document.getElementById('batchInputForm').addEventListener('clear', clearBatch);
 
 var batchSize;
+batchButtons.innerHTML = '';
 function saveBatch(e){
     batchSize = document.getElementById('batchSizeInput').nodeValue;
+    batchButtons.innerHTML = '<div class = "well">' + '<h6>Batch Left:' + batchSize + '</h6>' + '</div>';
 }
 
 function clearBatch(e){
     batchSize = 0;
 }
-
-batchButtons.innerHTML = '';
-
-batchButtons.innerHTML = '<div class = "well">' + '<h6>Batch Left:' + batchSize + '</h6>' + '</div>';
