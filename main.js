a025b380dda4e2a6f5334c029b0a328b5dfd93ea
Bobby <robbie234@gmail.com>
2019-03-10 19:33:39 -0400

    Trying to add button + size indicator

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('batchInputForm').addEventListener('submit', saveBatch);
+document.getElementById('batchInputForm').addEventListener('clear', clearBatch);
+
+var batchSize;
+function saveBatch(e){
+    batchSize = document.getElementById('batchSizeInput').nodeValue;
+}
+
+function clearBatch(e){
+    batchSize = 0;
+}
+
+batchButtons.innerHTML = '';
+
+batchButtons.innerHTML = '<div class = well>' + '<h6>Batch Left:' + batchSize + '</h6>' + '</div>';
