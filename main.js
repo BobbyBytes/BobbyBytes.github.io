740271f820380b501a3b8c3f2ee66e8de5180ccc
Bobby <robbie234@gmail.com>
2019-03-13 11:24:20 -0400

    testing makebuttons function

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function saveBatch(e){
     else {
         localStorage.setItem('localBS', batchSize);
     }
+makeBttons(batchSize);
 displayLeft();
 e.preventDefault();
 }
@@ -29,5 +30,43 @@ function displayLeft(e){
 }
 
 function getTotals(e){
+
+}
+
+function makeBttons(bLeft){
+    var tester = document.getElementById('temp');
+    batchSize = localStorage.getItem('localBS');
+    var worksize = batchSize;
+    var tenin = {
+        id: 10,
+        weight: 29.5
+    }
+    var ninein = {
+        id: 9,
+        weight: 19.5
+    }
+    var eightin = {
+        id: 8,
+        weight: 11.6
+    }
+    var sevenin = {
+        id: 7,
+        weight: 10.0
+    }
+    var sixin = {
+        id: 6,
+        weight: 5.1
+    }
+
+    var cakes =[tenin, ninein, eightin, sevenin, sixin];
+
+    for (i=0; i<6; i++){
+        if (bLeft > cakes[i].weight){
+            var id = cakes[i].id;
+            var weight = cakes[i].weight;
+            tester.innerHTML += '<div class="jumbotron">' + '<h2>Batch Left: ' + batchSize + '</h2>' +
+    '<a href="#" onclick="clearBatch()" class="btn btn-primary">' + id + '</a> ' + '</div>';
+        }
+    }
 
 }
