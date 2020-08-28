89d49370b7d95bd4891dbe3416248e93ccce5f54
BobbyBytes <robbie234@gmail.com>
2020-08-27 23:16:58 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -11,25 +11,23 @@ function startTimer(e) {
         var distance = timeToReach - now;
 
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Display the result in the element with id="demo"
-        document.getElementById("spaceForTimer").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
+        document.getElementById("spaceForTimer").innerHTML = minutes + "m " + seconds + "s ";
 
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("spaceForTimer").innerHTML = "Begin Cool Down";
+            document.getElementById("spaceForTimer").innerHTML = "Begin Cool Down" + distance;
         }
     }, 1000);
     e.preventDefault();
 }
 function clearTimer(e) {
-    document.getElementById("spaceForTimer").innerHTML = "";
+    document.getElementById("spaceForTimer").innerHTML = " ";
     e.preventDefault();
 }
 
