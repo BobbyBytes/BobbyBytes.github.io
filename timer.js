e2a265de0f7fbcbf0f8eaa45d121754ac30b6c36
BobbyBytes <robbie234@gmail.com>
2020-08-29 13:01:08 -0400

    Version 1

--- timer.js
+++ timer.js
@@ -1,9 +1,10 @@
 document.getElementById('SetTimesForm').addEventListener('submit', startHeatUpTimer);
 document.getElementById('SetTimesForm').addEventListener('reset', clearTimer);
 
-
+var canvas = document.getElementById("canvas");  
 var cancelSignalActive = false;
-doSomeGradient();
+fitToContainer(canvas);
+doHeatUpGradient(canvas, 0);
 function startHeatUpTimer(e) {
     var countDownTime = (document.getElementById('heatUpTimeInput').value) * 1000;
     var nowOutside = new Date().getTime();
@@ -19,11 +20,16 @@ function startHeatUpTimer(e) {
         // Find the distance between now and the count down date
         var distance = timeToReach - nowInside;
 
+        
         // Time calculations for days, hours, minutes and seconds
 
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+        var totalSecondsLeft = minutes * 60 + seconds
+        var percentageDone = 1 - (totalSecondsLeft / (countDownTime /1000))
+        doHeatUpGradient(canvas, percentageDone); 
+        //alert("seconds left" + totalSecondsLeft + "count dow time" + countDownTime +"percent"+ percentageDone);
         // Display the result in the element with id="demo"
         document.getElementById("spaceForTimer").innerHTML = "Heat Up.. " + minutes + "m " + seconds + "s ";
 
@@ -31,7 +37,8 @@ function startHeatUpTimer(e) {
         if (distance < 0) {
             clearInterval(x); 
             document.getElementById("spaceForTimer").innerHTML = "Begin Cool Down";
-            document.getElementById("jumbo").style.background = 'red';
+            document.getElementById("jumbo").style.webkitTransition = 'background-color 1.0s ease'
+            document.getElementById("jumbo").style.background = 'orange';              
             startCoolDownTimer();          
         }
     }, 100);
@@ -39,6 +46,11 @@ function startHeatUpTimer(e) {
 }
 
 function startCoolDownTimer() {
+    clearMyCanvas(canvas)
+    setTimeout(function () {
+        document.getElementById("jumbo").style.background = "#e9ecef";
+    }, 500);
+    
     var countDownTime = (document.getElementById('CoolDownTimeInput').value) * 1000;
     var nowOutside = new Date().getTime();
     var timeToReach = nowOutside + countDownTime
@@ -60,36 +72,52 @@ function startCoolDownTimer() {
 
         // Display the result in the element with id="demo"
         document.getElementById("spaceForTimer").innerHTML = "Cooling Down.. " + minutes + "m " + seconds + "s ";
-
+        var totalSecondsLeft = minutes * 60 + seconds
+        var percentageDone = 1 - (totalSecondsLeft / (countDownTime /1000))
+        doCoolDownGradient(canvas, percentageDone); 
         // If the count down is finished, write some text
         if (distance < 0) {
             document.getElementById("spaceForTimer").innerHTML = "Time's Up Partner ;)";
             clearInterval(x);
+            document.getElementById("jumbo").style.background = "orange";
             setTimeout(function () {
-            }, 500);
+                document.getElementById("jumbo").style.background = "#e9ecef";
+                setTimeout(function () {
+                    document.getElementById("jumbo").style.background = "orange";
+                    setTimeout(function () {
+                        document.getElementById("jumbo").style.background = "#e9ecef";
+                        setTimeout(function () {
+                            document.getElementById("jumbo").style.background = "orange";
+                            setTimeout(function () {
+                                document.getElementById("jumbo").style.background = "#e9ecef";
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+
         }
     }, 100);
 }
 
-function clearTimer(e) {
+function clearTimer(e) {  
     document.getElementById("spaceForTimer").innerHTML = "Ready..";
     cancelSignalActive = true
     document.getElementById("jumbo").style.background = "#e9ecef";
+    clearMyCanvas(canvas);
     e.preventDefault();
 }
 
-function doSomeGradient(){
-    var canvas = document.getElementById("canvas");  
+function doHeatUpGradient(canvas, percent){
 
     if (canvas.getContext)   
         {  
-        fitToContainer(canvas);
-        // var ctx = canvas.getContext("2d");         
-        // var gradient = ctx.createLinearGradient(10, 90, 00, 90);
-        // gradient.addColorStop(0, 'black');
-        // gradient.addColorStop(1, 'white');
-        // ctx.fillStyle = gradient;
-        // ctx.fillRect(10, 10, 200, 250);             
+        var ctx = canvas.getContext("2d");         
+        var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+        gradient.addColorStop(0, 'blue');
+        gradient.addColorStop(1, 'red');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, (percent * canvas.width), canvas.height);             
          }
 }
 function fitToContainer(canvas){
@@ -101,3 +129,25 @@ function fitToContainer(canvas){
     canvas.height = canvas.offsetHeight;
     canvas.style.background = 'grey';
   }
+
+  function clearMyCanvas(canvas){
+
+    if (canvas.getContext)   
+        {  
+        var ctx = canvas.getContext("2d");         
+            ctx.clearRect(0,0,canvas.width, canvas.height)
+         }
+}
+
+function doCoolDownGradient(canvas, percent){
+
+    if (canvas.getContext)   
+        {  
+        var ctx = canvas.getContext("2d");         
+        var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+        gradient.addColorStop(0, 'red');
+        gradient.addColorStop(1, 'green');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, (percent * canvas.width), canvas.height);             
+         }
+}
